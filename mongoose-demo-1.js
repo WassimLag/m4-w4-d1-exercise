c80daa3a7f1c2946279515ac4503df32c883eebd
wassim lagnaoui <wassimlagnaoui@wassims-MBP.attlocal.net>
2025-02-28 12:28:05 -0600

    Demo mongoose

---
+++ mongoose-demo-1.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  // we're connected!
+});
+
+const fruitSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const apple = new Fruit({ name: "Apple" });
+console.log(apple.name);
